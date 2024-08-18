d9cdc56d159156ce2f6a5b657fa8e6e93c1df046
amlanxbghn <amlanxconnect@gmail.com>
2024-08-18 06:38:59 +0530

    Product Card

---
+++ src/utils/data.js
@@ -0,0 +1,44 @@
+export const data = {
+    products: [
+        {
+            id: '1',
+            name: 'Grey Dress',
+            image: '/images/p1.jpg',
+            price: 120,
+            countInStock: 10,
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality dress'
+        },
+        {
+            id: '2',
+            name: 'Green Dress',
+            image: '/images/p2.jpg',
+            price: 90,
+            countInStock: 5,
+            rating: 4.8,
+            numReviews: 7,
+            description: 'high quality dress'
+        },
+        {
+            id: '3',
+            name: 'Floral Dress',
+            image: '/images/p3.jpg',
+            price: 50,
+            countInStock: 1,
+            rating: 4.6,
+            numReviews: 12,
+            description: 'high quality dress'
+        },
+        {
+            id: '4',
+            name: 'Black Dress',
+            image: '/images/p4.jpg',
+            price: 100,
+            countInStock: 15,
+            rating: 4.2,
+            numReviews: 5,
+            description: 'high quality dress'
+        },
+    ]
+}
